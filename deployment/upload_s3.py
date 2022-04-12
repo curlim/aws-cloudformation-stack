fbe1e43aca4c32e8a720d36fecd4bb0965db0d6a
Florian Zouhar <mail@zouhar.io>
2022-04-12 13:11:10 +0200

    added upload s3 logic

--- deployment/upload_s3.py
+++ deployment/upload_s3.py
@@ -1,2 +1,14 @@
+import os
+import boto3
+
+BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+
 if __name__ == '__main__':
 	print("upload s3")
+	s3 = boto3.resource("s3")
+	bucket = s3.Bucket(BUCKET_NAME)
+	bucket.upload_file(
+		Filename="proserve-cloudformation-stack.zip",
+		Key="proserve-cloudformation-stack.zip"
+	)
+
