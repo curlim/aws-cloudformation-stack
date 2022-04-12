229a31c82c344ecb9160b5285d1c2af61e2d92dd
Florian Zouhar <mail@zouhar.io>
2022-04-12 13:42:12 +0200

    ci fixes

--- deployment/upload_s3.py
+++ deployment/upload_s3.py
@@ -7,15 +7,19 @@ if __name__ == '__main__':
 	ref = os.getenv("GITHUB_REF").split('/')
 	if ref[1] != 'tags':
 		print("non tagged ref")
-		return
-	else:
 		version = ref[2]
+		env = "dev"
+	else:
+		print("tagged ref")
+		version = ref[2]
+		env = "release"
 	print(f"upload version {version} s3")
 	s3_client = boto3.client("s3")
 	s3 = boto3.resource("s3")
 	bucket = s3.Bucket(BUCKET_NAME)
+	key = f"proserve-cloudformation-stack/{version}.zip" if env == "release" else f"{env}/proserve-cloudformation-stack/{version}.zip"
 	bucket.upload_file(
 		Filename="proserve-cloudformation-stack.zip",
-		Key=f"proserve-cloudformation-stack/{version}.zip"
+		Key=key
 	)
 
