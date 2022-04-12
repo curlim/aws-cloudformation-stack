531f72b8435ed90c692242e076ae882efc9023b3
Florian Zouhar <mail@zouhar.io>
2022-04-12 13:22:42 +0200

    added aws default region

--- deployment/upload_s3.py
+++ deployment/upload_s3.py
@@ -5,6 +5,9 @@ BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
 if __name__ == '__main__':
 	print("upload s3")
+	s3_client = boto3.client("s3")
+	bucks = s3_client.list_buckets()
+	print(str(bucks))
 	s3 = boto3.resource("s3")
 	bucket = s3.Bucket(BUCKET_NAME)
 	bucket.upload_file(
