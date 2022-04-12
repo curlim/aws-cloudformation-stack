5bb1ed36767ca3997b05655ccb60c7d0d136852c
Florian Zouhar <mail@zouhar.io>
2022-04-12 13:35:19 +0200

    upload to s3 on tag only

--- deployment/upload_s3.py
+++ deployment/upload_s3.py
@@ -4,10 +4,14 @@ import boto3
 BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
 if __name__ == '__main__':
-	print("upload s3")
+	ref = os.getenv("GITHUB_REF").split('/')
+	if ref[1] != 'tags':
+		print("non tagged ref")
+		return
+	else
+		version = ref[2]
+	print(f"upload version {version} s3")
 	s3_client = boto3.client("s3")
-	bucks = s3_client.list_buckets()
-	print(str(bucks))
 	s3 = boto3.resource("s3")
 	bucket = s3.Bucket(BUCKET_NAME)
 	bucket.upload_file(
